3d024240f0f7d833b2f57a8ae3fd43a37b0400f1
cweijan <cweijan@163.com>
2023-04-12 16:12:54 +0800

    移除关闭按钮

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -89,14 +89,14 @@
           <a>{{ item }}</a>
         </div>
       </div>
-      <div class="close-btn" @click="close('btn')">
+      <!-- <div class="close-btn" @click="close('btn')">
         <svg width="30" height="30" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M8 8L40 40" :stroke="isNight?'#ffffff':'#000000'" stroke-width="4" stroke-linecap="round"
                 stroke-linejoin="round"/>
           <path d="M8 40L40 8" :stroke="isNight?'#ffffff':'#000000'" stroke-width="4" stroke-linecap="round"
                 stroke-linejoin="round"/>
         </svg>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
