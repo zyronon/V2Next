0916ea4188b6e4a2a2751174470c82be27982499
wangnianchao <wangnianchao@lerjin.com>
2023-07-11 10:27:53 +0800

    修复bug

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     monkey({
       entry: 'src/main.ts',
       userscript: {
+        version: '6.1',
         name: 'V2EX - 超级增强',
         icon: 'https://www.google.com/s2/favicons?sz=64&domain=v2ex.com',
         namespace: 'http://tampermonkey.net/',
@@ -28,7 +29,6 @@ export default defineConfig({
           'https://*.v2ex.com/recent*',
           'https://*.v2ex.com/go/*'
         ],
-        version: '6.0',
         description: '楼中楼回复(支持感谢数排序)、自动签到、快捷回复图片和表情、列表预览内容、点击帖子弹框展示详情、对用户打标签、回复上下文、记录上次阅读位置、自定义背景、使用 SOV2EX 搜索、正文超长自动折叠、划词 base64 解码、一键@所有人,@管理员、操作按钮(感谢、收藏、回复、隐藏)异步请求、支持黑暗模式',
         author: 'zyronon',
         license: 'GPL License',
