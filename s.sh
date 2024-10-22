82ec9f36bb6a69b191d32f347bdb056db51e855c
zyronon <zyronon@163.com>
2024-10-22 14:18:31 +0800

    save

--- s.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-git filter-branch -f --env-filter '
-export GIT_COMMITTER_NAME="zyronon"
-export GIT_COMMITTER_EMAIL="zyronon@163.com"
-export GIT_AUTHOR_NAME="zyronon"
-export GIT_AUTHOR_EMAIL="zyronon@163.com"
-'
